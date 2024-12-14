3aebb53c32b3761e769b2395c4a7950142f54429
Konstantin Matsa <konstantinmaca@gmail.com>
2024-12-14 15:37:09 +0500

    Fix not detecting my laptop.

--- generate-config.rb
+++ generate-config.rb
@@ -1,6 +1,7 @@
 GENERATED_CONFIG_DIR = "#{`echo $HOME`.chomp}/.config/kak/generated"
-EASY_LANGS = 'c cpp cc ruby'.split' '
-HARD_LANGS = 'rust ruby tex latex'.split' '
+EASY_LANGS = 'ruby'.split' '
+HARD_LANGS = 'c cpp cc rust tex latex'.split' '
+EASY_LANGS_RE = EASY_LANGS.join'|'
 ALL_LANGS_RE = (EASY_LANGS + HARD_LANGS).join'|'
 
 
@@ -8,12 +9,13 @@ puts "[Azzy's Generated Config] Ensure the directory for generated config files 
 system("mkdir -p #{GENERATED_CONFIG_DIR}")
 
 OPTION = 'lspEnableWindowHook'
-print "[Azzy's Generated Config] Generating config by option `#{OPTION}`... "
+puts "[Azzy's Generated Config] Generating config by option `#{OPTION}`... "
 text = case ARGV[0]
 when OPTION
-  if `whoami` == 'azzimoda'
+  if `whoami`.strip == 'azzimoda'
+    print "[Azzy's Generated Config] Using easy language servers: (#{EASY_LANGS_RE})"
     "echo -debug 'Language some language servers will be ignored because of your weak laptop.'
-hook global WinSetOption filetype=(#{EASY_LANGS}) %{
+hook global WinSetOption filetype=(#{EASY_LANGS_RE}) %{
     set-option window lsp_auto_highlight_references true
     set-option window lsp_hover_anchor false
     lsp-auto-hover-enable
@@ -21,6 +23,7 @@ hook global WinSetOption filetype=(#{EASY_LANGS}) %{
     lsp-enable-window
 }"
   else
+    print "[Azzy's Generated Config] Using all language servers: (#{ALL_LANGS_RE})"
     "hook global WinSetOption filetype=(#{ALL_LANGS_RE}) %{
     set-option window lsp_auto_highlight_references true
     set-option window lsp_hover_anchor false
